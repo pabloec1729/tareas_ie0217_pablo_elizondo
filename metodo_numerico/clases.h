960731f2f111fff11fa5a3f572d665623412be73
pabloec1729 <pabloelizond@gmail.com>
2022-09-14 23:44:08 -0600

    Método funcionando, resolviendo una función

--- metodo_numerico/clases.h
+++ metodo_numerico/clases.h
@@ -1,22 +1,27 @@
-extern float suma(float,float );
-
-
 
 class funcion{
     private:
         float punto_actual;
+        float punto_actual_up;
+        float punto_actual_down;
+
         float error_requerido;
         float punto_inicial;
 
         float valor_funcion;
+        float valor_funcion_up;
+        float valor_funcion_down;
+
         float valor_derivada;
         float punto_anterior;
-        float error_obtenido;
+        float error_obtenido = 1;
     public:
 
         funcion();
+//setters
+        void seterror_requerido(float);
 
-        void setvalor_funcion();
+        void setvalor_funcion(float);
 
         void setvalor_derivada();
 
@@ -25,31 +30,66 @@ class funcion{
         void setpunto_anterior();
 
         void setpunto_inicial(float);
-
+//getters
         float getpunto_actual();
+
+        float getvalor_funcion();
+
+        float getvalor_derivada();
 
         float calcular_error();
 
         float obtener_raiz();
 
+
+
 };
 
+//funcion::funcion(float _a, _b, _c, x){
+//    funcion = _a*pow(_x,2)+_b*_x + _c;
+//}
+
 funcion::funcion(){
-}
-
-void funcion::setvalor_funcion(){
-    valor_funcion = pow(punto_actual,2) + punto_actual*7;
-}
-
-
-void funcion::setpunto_actual(){
-
-    punto_actual = punto_actual - valor_funcion/valor_derivada;
 }
 
 void funcion::setpunto_inicial(float _punto_inicial){
     punto_actual = _punto_inicial;
 }
+
+void funcion::seterror_requerido(float _error_requerido){
+    error_requerido = _error_requerido;
+}
+
+void funcion::setvalor_funcion(float _punto){
+    valor_funcion = 4*pow(_punto,2) -9*_punto + 3;
+    valor_funcion_up = pow(_punto+0.0001,3) - 1;
+    valor_funcion_down = pow(_punto-0.0001,3) - 1;
+}
+
+void funcion::setvalor_derivada(){
+    valor_derivada = (valor_funcion_up - valor_funcion_down)/0.0002;
+}
+
+void funcion::setpunto_actual(){
+    while (abs(error_obtenido) > error_requerido){
+        setvalor_funcion(punto_actual);
+        setvalor_derivada();
+        punto_anterior = punto_actual;
+        punto_actual = punto_actual - (valor_funcion/valor_derivada);
+        error_obtenido = abs((punto_actual - punto_anterior)/punto_actual);
+    }
+}
+//
+//void funcion::setpunto_anterior(){
+//    punto_anterior = _punto_actual;
+//}
+
+
+
+
+
+
+
 
 
 float funcion::calcular_error(){
@@ -60,3 +100,10 @@ float funcion::getpunto_actual(){
     return punto_actual;
 }
 
+float funcion::getvalor_funcion(){
+    return valor_funcion;
+}
+
+float funcion::getvalor_derivada(){
+    return valor_derivada;
+}
--- metodo_numerico/main.cpp
+++ metodo_numerico/main.cpp
@@ -29,7 +29,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <math.h>
-#include "funciones.h"
+#include "clases.h"
+#include <cmath>
 using namespace std;
 int variable_global=10;
 
@@ -37,16 +38,26 @@ extern float suma(float,float);
 
 int main(void)
 {
-   int variable_global=30;
-    float val_in = 9;
+    float val_in = 1;
+    float error_requerido = 0.001;
     funcion funcion1;
 
-   cout << "Establezca valor de prueba inicial:\n";
-   cin >> val_in;
-   funcion1.setpunto_inicial(val_in);
-   cout << funcion1.getpunto_actual()<<endl;
-   cout << "Seleccione los parametros";
 
-   variable_global++;
-   getchar();
+   cout << "Establezca valor de prueba inicial:\n";
+//   cin >> val_in;
+//   cout << "Ingrese el error maximo deseado:\n";
+//   cin >> error_requerido;
+
+//   funcion1.setpunto_inicial(val_in);
+   funcion1.setvalor_funcion(val_in);
+   funcion1.setvalor_derivada();
+   funcion1.seterror_requerido(error_requerido);
+   funcion1.setpunto_inicial(val_in);
+
+   cout << funcion1.getpunto_actual()<<endl;
+   cout << "El valor de la funcion es "<<funcion1.getvalor_funcion()<<endl;
+   cout << "El valor de la derivada es "<<funcion1.getvalor_derivada()<<endl;
+   funcion1.setpunto_actual();
+   cout << "El cero esta en " <<funcion1.getpunto_actual()<<endl;
+
 }//________________________________________
