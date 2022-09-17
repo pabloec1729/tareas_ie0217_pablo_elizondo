3bd3739881005d6933d3cdf577920c0e0f2ac726
pabloec1729 <pabloelizond@gmail.com>
2022-09-17 17:45:31 -0600

    Ćódigo terminado y funcionando correctamente

--- metodo_numerico/clases.h
+++ metodo_numerico/clases.h
@@ -1,115 +1,128 @@
 
+
 class funcion{
     private:
 //parametros de las funciones algebraicas
-        float a;
-        float b;
-        float c;
-        float d;
+        double a;
+        double b;
+        double c;
+        double d;
 
-        std::string func_tipo = "edfs";
+        std::string func_tipo = "";
 
-        float punto_actual;
-        float punto_actual_up;
-        float punto_actual_down;
+        double punto_actual;
+        double punto_actual_up;
+        double punto_actual_down;
 
-        float error_requerido;
-        float punto_inicial;
+        double error_requerido;
+        double punto_inicial;
 
-        float valor_funcion;
-        float valor_funcion_up;
-        float valor_funcion_down;
+        double valor_funcion;
+        double valor_funcion_up;
+        double valor_funcion_down;
 
-        float valor_derivada;
-        float punto_anterior;
-        float error_obtenido = 1; //Iniciamos asumiendo un error obtenido grande
+        double valor_derivada;
+        double punto_anterior;
+        double error_obtenido = 1; //Iniciamos asumiendo un error obtenido grande
     public:
 
         funcion();
 //setters
-        void setfunc_tipo(std::string);
+        void setfunc_tipo(std::string); //el usuario puede ponerle una etiqueta a la funcion desde el main
 
-        void seterror_requerido(float);
+        void seterror_requerido(double); //Al usuario se le solicita este dato como un porcentaje, asi que debe dividirse entre 100
 
-        void setvalor_funcion(float);
+        void setvalor_funcion(double);  //Calcula el valor de la funcion en el valor de prueba "p" y en otros 2 valores cercanos ((p + dp) y (p-dp), siendo "dp" un valor muy pequeño)
 
-        void setvalor_derivada();
+        void setvalor_derivada();  //Usando los valores dados por el metodo anterior calcula la pendiente de la linea recta entre los puntos r = (p + dp,f(p + dp)) y g = (p - dp,f(p - dp))
 
-        void setpunto_actual();
+        void setpunto_actual();  //Implementacion del metodo de Newton-Raphson
 
-        void setpunto_anterior();
+        void setpunto_anterior(); //Guarda el valor de prueba anterior
 
-        void setpunto_inicial(float);
+        void setpunto_inicial(double); //establece el valor de prueba inicial
 
-        void setparametros(float,float,float,float);
+        void setparametros(double,double,double,double); //Define los parametros de las funciones algebraicas de la forma f(x) = a*x^3 + b*x^2 + c*x + d , se pueden modificar desde el main
 //getters
         std::string getfunc_tipo();
 
-        float getpunto_actual();
+        double getpunto_actual();
 
-        float getvalor_funcion();
+        double getvalor_funcion();
 
-        float getvalor_derivada();
+        double getvalor_derivada();
 
-        float calcular_error();
+        double calcular_error();
 
-        float obtener_raiz();
+        double obtener_raiz();
 };
 
 funcion::funcion(){
 }
 
-void funcion::setpunto_inicial(float _punto_inicial){
+void funcion::setpunto_inicial(double _punto_inicial){
     punto_actual = _punto_inicial;
 }
 
-void funcion::seterror_requerido(float _error_requerido){
-    error_requerido = _error_requerido;
+void funcion::seterror_requerido(double _error_requerido){
+    error_requerido = _error_requerido/100;
 }
 
-void funcion::setparametros(float _a, float _b, float _c, float _d){
+void funcion::setparametros(double _a, double _b, double _c, double _d){
     a = _a;
     b = _b;
     c = _c;
     d = _d;
 }
 
-void funcion::setvalor_funcion(float _punto){
+void funcion::setvalor_funcion(double _punto){
+    //Se escogio 0.0001 como un valor muy cercano a cero con el cual se pueden obtener dos puntos uno a la derecha y otro a la izquierda del valor de prueba
+
     valor_funcion = a*pow(_punto,3) + b*pow(_punto,2) + c*_punto + d;
     valor_funcion_up = a*pow(_punto + 0.0001,3) + b*pow(_punto+0.0001,2) + c*(_punto+0.0001) + d;
     valor_funcion_down = a*pow(_punto - 0.0001,3) + b*pow(_punto-0.0001,2) + c*(_punto-0.0001) + d;
 }
 
 void funcion::setvalor_derivada(){
+    //La derivada se obtiene linealizando alrededor del punto de prueba y aplicando la definicion pendiente, esto permite que desde el main
+    //se puedan modificar y crear cuantas funciones se quieran sin tener que preocuparse por definir su derivada, siempre y cuando la funcion sea algebraica
     valor_derivada = (valor_funcion_up - valor_funcion_down)/0.0002;
 }
-//
+
 
 void funcion::setpunto_actual(){
     int i = 1;
     int while_count = 0;
+    int max_iteraciones = 30;   //Se puede modificar este parametro en caso de que se requiera mucha precision
     std::cout<<"Funcion "<<func_tipo<<std::endl;
-    std::cout<<"F(x) = "<<a<<"x^3 + "<<b<<"x^2 + "<<c<<"x + "<<d<<std::endl;
+    std::cout<<"F(x) = "<<a<<"x^3 + "<<b<<"x^2 + "<<c<<"x + "<<d<<"\n"<<std::endl;
 
-    while (abs(error_obtenido) > error_requerido){
-        std::cout<<"Probando x = "<<punto_actual<<std::endl;
+    std::cout<<"Iteracion No."<<i<<": Probando x = "<<punto_actual<<std::endl;
+    while ((abs(error_obtenido) > error_requerido) && (while_count < max_iteraciones)){
+
         setvalor_funcion(punto_actual);
         setvalor_derivada();
         punto_anterior = punto_actual;
-        std::cout<<"punto actual iteracion "<<i<<": "<<punto_actual<<std::endl;
         punto_actual = punto_actual - (valor_funcion/valor_derivada);
         i = i + 1;
         while_count = while_count + 1;
         error_obtenido = abs((punto_actual - punto_anterior)/punto_actual);
-
-
+        std::cout<<"Iteracion No."<<i<<"\nProbando x = "<<punto_actual<<std::endl;
+        std::cout<<"Error obtenido: "<<error_obtenido*100<<"%\n"<<std::endl;
 
     }
-    if (while_count == 1){
 
+    if (while_count == 1){
         i = 1;
     }
-    std::cout<<"X = "<<punto_actual<<" es una solucion, se necesitaron "<<i<<" iteraciones"<<std::endl;
+
+    if (while_count < max_iteraciones){ //Se establece un numero maximo de iteraciones ya que en caso de que no haya solucion el programa no terminaria
+        std::cout<<"\nSolucion x = "<<punto_actual<<"\nIteraciones: "<<i<<"\nError: "<<error_obtenido*100<<"%"<<std::endl;
+    }
+    else {
+        std::cout<<"No hay soluciones reales tras "<<max_iteraciones<<" iteraciones"<<std::endl;
+    }
+
 }
 
 void funcion::setfunc_tipo(std::string _func_tipo){
@@ -120,34 +133,30 @@ std::string funcion::getfunc_tipo(){
     return func_tipo;
 }
 
-float funcion::calcular_error(){
+double funcion::calcular_error(){ //calculo del error
     error_obtenido = (punto_actual - punto_anterior)/punto_actual;
 }
 
-float funcion::getpunto_actual(){
+double funcion::getpunto_actual(){
     return punto_actual;
 }
 
-float funcion::getvalor_funcion(){
+double funcion::getvalor_funcion(){
     return valor_funcion;
 }
 
-float funcion::getvalor_derivada(){
+double funcion::getvalor_derivada(){
     return valor_derivada;
 }
 
+
+//clases hijas de la clase funcion que son usadas en el main para resolver 3 ecuaciones de distinto grado
 class grado3 : public funcion{
-
-
 
 };
 
 class grado2 : public funcion{
-
-
 };
 
 class grado1 : public funcion{
-
-
 };
--- metodo_numerico/main.cpp
+++ metodo_numerico/main.cpp
@@ -12,40 +12,54 @@
 #include "clases.h"
 #include <cmath>
 #include <string.h>
+
+
 using namespace std;
 
 
 int main(void)
 {
-    float val_in = 98;
-    float error_requerido = 0.001;
-
-
-//    funcion funcion1;
+    double val_in; //Guarda el primer valor de prueba
+    double error_maximo; //Guarda el erro maximo solicitado por el usuario
 
     grado3 hiperbolica;
     grado2 parabola;
+    grado2 parabola_sin_solucion;
     grado1 lineal;
+
+
+
+    cout <<"Ingrese un valor de prueba inicial: "; cin>>val_in;
+    cout <<"Ingrese el porcentaje de error maximo deseado: "; cin>>error_maximo;
 
    cout <<"\n=================Nueva funcion======================\n"<<endl;
    lineal.setfunc_tipo("lineal");
-   lineal.setparametros(0,0,3,7);
+   lineal.setparametros(0,0,3,7); //parametros a,b,c y d respectivamente
    lineal.setpunto_inicial(val_in);
-   lineal.seterror_requerido(error_requerido);
-   lineal.setpunto_actual();
+   lineal.seterror_requerido(error_maximo);
+   lineal.setpunto_actual();    //Metodo de Newton-Raphson, calculando la solucion comparando entre los puntos de prueba obtenidos
 
    cout <<"\n=================Nueva funcion======================\n"<<endl;
    parabola.setfunc_tipo("cuadratica");
-   parabola.setparametros(0,1,9,3);
+   parabola.setparametros(0,1,2,1); //parametros a,b,c y d respectivamente
    parabola.setpunto_inicial(val_in);
-   parabola.seterror_requerido(error_requerido);
-   parabola.setpunto_actual();
+   parabola.seterror_requerido(error_maximo);
+   parabola.setpunto_actual(); //Metodo de Newton-Raphson, calculando la solucion comparando entre los puntos de prueba obtenidos
 
    cout <<"\n=================Nueva funcion======================\n"<<endl;
    hiperbolica.setfunc_tipo("hiperbolica");
-   hiperbolica.setparametros(1,2,3,4);
-   hiperbolica.setvalor_funcion(val_in);
-   hiperbolica.seterror_requerido(error_requerido);
-   hiperbolica.setpunto_actual();
+   hiperbolica.setparametros(1,2,3,4); //parametros a,b,c y d respectivamente
+   hiperbolica.setpunto_inicial(val_in);
+   hiperbolica.seterror_requerido(error_maximo);
+   hiperbolica.setpunto_actual(); //Metodo de Newton-Raphson, calculando la solucion comparando entre los puntos de prueba obtenidos
+
+
+    cout <<"\n=================Nueva funcion======================\n"<<endl; //Probando una funcion sin solucion real
+   parabola_sin_solucion.setfunc_tipo("cuadratica");
+   parabola_sin_solucion.setparametros(0,1,1,1); //parametros a,b,c y d respectivamente
+   parabola_sin_solucion.setpunto_inicial(val_in);
+   parabola_sin_solucion.seterror_requerido(error_maximo);
+   parabola_sin_solucion.setpunto_actual(); //Metodo de Newton-Raphson, calculando la solucion comparando entre los puntos de prueba obtenidos
+
 
 }//________________________________________
